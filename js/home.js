38876c1f32d8f65184c2d160bcf541db3c896e25
akinnez <oyetunjiidris@gmail.com>
2021-09-15 19:34:07 +0100

    changes done

--- js/home.js
+++ js/home.js
@@ -27,25 +27,13 @@ function light() {
             
             var i = Math.floor(Math.random() * Recent.length )
             
-    document.getElementById(
-      'pageBanner'
-    ).style.backgroundImage = `url(${Recent[i].Image})`;
-  bannerDetails.innerHTML += 
-        `<div>
-          <h1>${Recent[i].title}</h1>
-              <div class="textContainer">
-                      <span>
-                      ${Recent[i].desc.substr(0, 220) + '...'}
-                      </span>
-          </div>
-          <div>
-              <button class="btn mt-3">Play Trailer</button>
-          </div>
-          </div>` 
+            
+          movieBanner(Recent,i)
+          displayBanner(Recent,i)
           moviesDisplay(Upcoming);
           recentMovies(Recent);
           PopularMovies(Popular)
-          loadingLazy()
+          // loadingLazy()
           }
             )
           .catch((err) => {
@@ -58,12 +46,46 @@ function light() {
     function trailer(params) {
       
     }
+
+    function movieBanner(Recent,i) {
+      document.getElementById(
+        'pageBanner'
+      ).style.backgroundImage = `url(${Recent[i].Image})`;
+    bannerDetails.innerHTML += 
+          `<div>
+            <h1>${Recent[i].title}</h1>
+                <div class="textContainer">
+                        <span>
+                        ${Recent[i].desc.substr(0, 220) + '...'}
+                        </span>
+            </div>
+            <div>
+                <button class="btn mt-3">Play Trailer</button>
+            </div>
+            </div>`
+    }
+    function displayBanner(Recent,i) {
+      bannerDetails1.innerHTML += 
+          `<div>
+            <h1>${Recent[i].title}</h1>
+                <div class="textContainer">
+                        <span>
+                        ${Recent[i].desc.substr(0, 220) + '...'}
+                        </span>
+            </div>
+            <div>
+                <button class="btn mt-3">Play Trailer</button>
+            </div>
+            </div>`
+      
+    }
+
   function moviesDisplay(newMovie) {
     // let date = new Date()
     // console.log(date.)
     for (let index = 0; index < newMovie.length; index++) {
       ul.innerHTML += ` <li class='imgList'>
-          <img data-src="${newMovie[index].Image}" alt="" class="image">
+          <img src="${newMovie[index].Image}" alt="" class="image">
           <div id='overlay'>
             <div>
               <button class='btn' id='button' onclick="getInfo(newMovie[${index}])">Movie Details</button>
@@ -80,7 +102,7 @@ function light() {
     // console.log(date.)
     for (let index = 0; index < recentMovie.length; index++) {
       ull.innerHTML += ` <li class='imgList'>
-          <img data-src="${recentMovie[index].Image}" alt="" class="image">
+          <img src="${recentMovie[index].Image}" alt="" class="image">
           <div id='overlay'>
             <div>
               <button class='btn' id='button' onclick="getInfo(recentMovie[${index}])">Movie Details</button>
@@ -96,10 +118,10 @@ function light() {
     // console.log(date.)
     for (let index = 0; index < popularMovie.length; index++) {
       ulll.innerHTML += ` <li class='imgList'>
-          <img data-src="${popularMovie[index].Image}" alt="" class="image">
+          <img src="${popularMovie[index].Image}" alt="" class="image">
           <div id='overlay'>
             <div>
-              <button class='btn' id='button' onclick="getInfo(popularMovie[${index}])">Movie Details</button>
+              <button class='btn' id='button' onclick="getInfo(${index})">Movie Details</button>
               </div>
             </div>
   </li>      
@@ -126,29 +148,29 @@ function light() {
     console.log(mine);
   }
 
-  let loadingLazy = ()=> {
-    var elements = document.querySelectorAll('img[data-src]');
-    var index = 0;
+  // let loadingLazy = ()=> {
+  //   var elements = document.querySelectorAll('img[data-src]');
+  //   var index = 0;
     
-    var lazyLoad = ()=> {
-      if (index >= elements.length) return;
-      var item = elements[index]; 
-      console.log(item.offsetWidth);
-      if (this.scrollY + this.innerHeight > item.offsetTop) { 
-        var src = item.getAttribute('data-src');
-        item.src = src;
-        item.addEventListener('load', ()=> {
-          item.removeAttribute('data-src');
-        });
-        index++;
-        lazyLoad();
-      }
-    };
-    var init = function () {
-      window.addEventListener('scroll', lazyLoad);
-      lazyLoad();
-    };
-    return init();
-  }
-  loadingLazy()
-  ;
+  //   var lazyLoad = ()=> {
+  //     if (index >= elements.length) return;
+  //     var item = elements[index]; 
+  //     console.log(item.offsetWidth);
+  //     if (this.scrollY + this.innerHeight > item.offsetTop) { 
+  //       var src = item.getAttribute('data-src');
+  //       item.src = src;
+  //       item.addEventListener('load', ()=> {
+  //         item.removeAttribute('data-src');
+  //       });
+  //       index++;
+  //       lazyLoad();
+  //     }
+  //   };
+  //   var init = function () {
+  //     window.addEventListener('scroll', lazyLoad);
+  //     lazyLoad();
+  //   };
+  //   return init();
+  // }
+  // loadingLazy()
+  // ;
